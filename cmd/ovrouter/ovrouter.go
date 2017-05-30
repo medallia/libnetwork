24aac5266def32ec7bde0edbb1ed61f867bfca67
Mauricio Garavaglia <mauricio@medallia.com>
2017-05-30 11:32:32 -0300

    add routed network driver

--- cmd/ovrouter/ovrouter.go
+++ cmd/ovrouter/ovrouter.go
@@ -38,6 +38,14 @@ func (ep *endpoint) Interface() driverapi.InterfaceInfo {
 	return nil
 }
 
+func (ep *endpoint) ExtraAddresses() []*net.IPNet {
+	return nil
+}
+
+func (ep *endpoint) SetExtraIPAddresses(addresses []*net.IPNet) error {
+	return nil
+}
+
 func (ep *endpoint) SetMacAddress(mac net.HardwareAddr) error {
 	if ep.mac != nil {
 		return types.ForbiddenErrorf("endpoint interface MAC address present (%s). Cannot be modified with %s.", ep.mac, mac)
--- drivers_linux.go
+++ drivers_linux.go
@@ -7,6 +7,7 @@ import (
 	"github.com/docker/libnetwork/drivers/null"
 	"github.com/docker/libnetwork/drivers/overlay"
 	"github.com/docker/libnetwork/drivers/remote"
+	"github.com/docker/libnetwork/drivers/routed"
 )
 
 func getInitializers(experimental bool) []initializer {
@@ -17,6 +18,7 @@ func getInitializers(experimental bool) []initializer {
 		{null.Init, "null"},
 		{remote.Init, "remote"},
 		{overlay.Init, "overlay"},
+		{routed.Init, "routed"},
 	}
 
 	if experimental {
